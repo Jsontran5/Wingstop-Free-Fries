2513f497c70aaed9ffd32801ce38510f7d2049eb
Jason Tran <jsontran5@gmail.com>
2023-08-31 05:18:33 -0700

    added sessions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
 app = Flask(__name__)
+app.secret_key = "hello"	
+app.permanent_session_lifetime = timedelta(days=3)
 
 @app.route('/') 
 def home():
@@ -8,15 +12,32 @@ def home():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form['nm']
-        return redirect(url_for('user', usr=user))
+        session['user'] = user
+        flash(f"Login Successful!", "info")
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            flash(f"Already Logged In!", "info")
+            return redirect(url_for("user"))
+        
         return render_template("login.html")
 
-@app.route('/<usr>')
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash(f"You are not logged in!", "info")
+        return redirect(url_for("login"))
 
+@app.route('/logout')
+def logout():
+    flash("You have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
