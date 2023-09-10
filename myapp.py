909dceab096d073afc28ca32055cae376d3501f7
Jason Tran <jsontran5@gmail.com>
2023-09-10 02:52:49 -0700

    second commit

---
+++ myapp.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for
+from blazefunction import blaze_pizza_survey
+from pandafunction import panda_survey
+from wingstopoptimizedfunction import wingstop_survey
+from rubiosfunction import rubios_survey
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/submit', methods=['POST'])
+    def submit():
+        email = request.form.get('email')
+        # Default message in case no option is selected
+        result = "Please select an option."
+
+        # Check if a specific option was selected and call the respective function
+        selected_option = request.form.get('option')
+        if selected_option == 'wingstop':
+            result = wingstop_survey(email)
+        elif selected_option == 'rubios':
+            result = rubios_survey()
+        elif selected_option == 'pandaexpress':
+            result = panda_survey(email)
+        elif selected_option == 'blazepizza':
+            result = blaze_pizza_survey(email)
+
+        return redirect(url_for('result', result=result))
+
+    @app.route('/wingstop')
+    def wingstop():
+        return render_template('wingstop.html', option="Wingstop")
+
+    @app.route('/rubios')
+    def rubios():
+        return render_template('rubios.html', option="Rubio's")
+
+    @app.route('/pandaexpress')
+    def pandaexpress():
+        return render_template('pandaexpress.html', option="Panda Express")
+
+    @app.route('/blazepizza')
+    def blazepizza():
+        return render_template('blazepizza.html', option="Blaze Pizza")
+
+    @app.route('/result')
+    def result():
+        result = request.args.get('result', 'Please select an option.')
+        return render_template('result.html', result=result)
+
+    return app
+
+# if __name__ == '__main__':
+#     create_app().run(debug=True)
--- second.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, session
-
-
-second = Blueprint('second', __name__, static_folder= 'static',template_folder='templates')
-
-@second.route('/home')
-@second.route('/')
-def home():
-    return render_template("home.html")
