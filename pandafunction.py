4046aad627cba67185fbfb777f394aaa50444169
Jason Tran <jsontran5@gmail.com>
2023-09-26 20:33:49 -0700

    Clarified each survey

--- pandafunction.py
+++ pandafunction.py
@@ -63,7 +63,7 @@ def panda_survey(email):
     next_button.click()
    
     if driver.current_url.startswith("https://www.pandaguestexperience.com/Finish.aspx"):
-        result = "Success! - Panda Express"
+        result = "Success! Coupon sent to your email. - Panda Express"
         driver.quit()
         return result
 
