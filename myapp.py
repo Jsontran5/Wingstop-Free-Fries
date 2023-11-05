68a967dbb0c4c672d052699a8521a4c374a9f676
Jason Tran <jsontran5@gmail.com>
2023-11-05 03:21:02 -0800

    edited meta data

--- myapp.py
+++ myapp.py
@@ -35,6 +35,7 @@ def create_app():
             result = blaze_pizza_survey(email)
 
         return redirect(url_for('result', result=result))
+    
     @app.route('/robots.txt')
     def static_from_root():
         return send_from_directory(app.static_folder, request.path[1:])
