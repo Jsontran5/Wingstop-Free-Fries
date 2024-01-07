e3514fa7c4ade57558e295481e289641ff8af744
Jason Tran <jsontran5@gmail.com>
2024-01-06 23:50:43 -0800

    reopened rubios

--- rubiosfunction.py
+++ rubiosfunction.py
@@ -17,7 +17,7 @@ def rubios_survey():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     #chrome_options.add_argument("--incognito")
-    #chrome_options.binary_location = '/opt/render/project/.render/chrome/opt/google/chrome' #for render.com
+    chrome_options.binary_location = '/opt/render/project/.render/chrome/opt/google/chrome' #for render.com
 
     driver = webdriver.Chrome(options=chrome_options)
     def wait_for_new_url(driver, previous_url, timeout=5):
