4ce221963f40e2e2e0c61da3c582327c8f377dde
Jason Tran <jsontran5@gmail.com>
2026-09-08 18:50:08 -0700

    more chrome options

--- pandafunction.py
+++ pandafunction.py
@@ -18,6 +18,8 @@ def panda_survey(email):
     options = uc.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--incognito")
     
